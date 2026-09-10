aeafc1432cc703929e34cd190d14bc74d8f28fd5
seifalfayyad <seifalfayyad@gmail.com>
2026-09-10 16:21:43 +0800

    Update config.php

--- jewellery_app/config.php
+++ jewellery_app/config.php
@@ -5,6 +5,6 @@ define('DB_PASS', '');
 define('DB_NAME', 'jewellery_shop');
 
 
-define('METALPRICE_API_KEY', '347983af0143aed08c600100a6dd5b8e');
-define('ABSTRACT_API_KEY', '659e53d96cdd423b9bf2c4aa5962ea3f');
+define('METALPRICE_API_KEY', 'SEIF_METALPRICE_API_KEY');
+define('ABSTRACT_API_KEY', 'SEIF_ABSTRACT_API_KEY');
 ?>
